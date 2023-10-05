31e7fbff0d1dcbf139acd9f5aad2945172024c40
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-10-04 23:10:56 -0300

    Update Backspaces in string.js

--- Backspaces in string.js
+++ Backspaces in string.js
@@ -11,3 +11,15 @@ Examples
 ""              ==>  ""
 */
 
+function cleanString(s) {
+  s = s.split``;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "#") {
+      s[i] = "";
+      s[i - 1] = "";
+      s = s.filter(v => v !== "");
+      i -= 2;
+    }
+  }
+  return s.join``;
+}

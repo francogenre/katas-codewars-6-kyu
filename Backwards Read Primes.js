e83298b3f93ce78b34c8d042144dd9c19e3c3db1
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-10-05 22:10:50 -0300

    Update Backwards Read Primes.js

--- Backwards Read Primes.js
+++ Backwards Read Primes.js
@@ -59,3 +59,28 @@ function isPrime(n) {
   }
   return true;
 }
+
+/*
+const isPrime = num => {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+};
+
+const isReversePrime = num => {
+    let reverseNum = +num.toString().split('').reverse().join('');
+    if (num !== reverseNum) return isPrime(reverseNum);
+    else return false;
+}
+
+const backwardsPrime = (start, stop) => {
+    let result = [];
+    for (let num = start; num <= stop; num++) {
+        if (num % 2 !== 0) {
+            if (isPrime(num) && isReversePrime(num)) result.push(num);
+        }
+    }
+    return result;
+}
+*/

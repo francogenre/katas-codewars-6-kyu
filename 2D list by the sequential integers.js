95979a4894f068b9a48736632c286953c2a64dd8
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-10-10 22:54:16 -0300

    Update 2D list by the sequential integers.js

--- 2D list by the sequential integers.js
+++ 2D list by the sequential integers.js
@@ -11,3 +11,18 @@ Note:
 0 <=  the number of columms <= 1000
 */
 
+function make2dList(head, row, col){
+  let arr = [];
+  let tempArr =[];
+  for (let i = 0; i < row; i++){
+    tempArr = [];
+    for (let j = 0;j < col; j++){
+  
+    tempArr.push(head++);
+    
+    }
+    arr.push(tempArr);
+  }
+  
+	return head > 10000000004?[] : arr;
+}

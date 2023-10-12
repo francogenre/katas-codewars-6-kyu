2231fe942f9189e6be5d00dc77f89d8812387ece
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-10-11 23:44:24 -0300

    Update Decipher this!.js

--- Decipher this!.js
+++ Decipher this!.js
@@ -14,3 +14,11 @@ decipherThis('72olle 103doo 100ya'); // 'Hello good day'
 decipherThis('82yade 115te 103o'); // 'Ready set go'
 */
 
+function decipherThis(str) {
+  return str.split(" ")
+    .map(w =>
+      w.replace(/^\d+/, c => String.fromCharCode(c))
+       .replace(/^(.)(.)(.*)(.)$/, "$1$4$3$2")
+    )
+    .join(" ")
+}

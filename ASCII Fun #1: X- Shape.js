caa3f66e085dacfb581ba2812beef6fe3c5efad5
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-10-12 23:59:35 -0300

    Create ASCII Fun #1: X- Shape.js

---
+++ ASCII Fun #1: X- Shape.js
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+You will get an odd integer n (>= 3) and your task is to draw an X. Each line is separated with \n.
+
+Use the following characters: ■ □ For Ruby, Crystal and PHP: whitespace and o
+
+Examples
+
+                                     ■□□□■
+            ■□■                      □■□■□
+  x(3) =>   □■□             x(5) =>  □□■□□
+            ■□■                      □■□■□
+                                     ■□□□■
+
+Series: ASCII Fun
+ASCII Fun #1: X-Shape
+ASCII Fun #2: Funny Dots
+ASCII Fun #3: Puzzle Tiles
+ASCII Fun #4: Build a pyramid
+*/

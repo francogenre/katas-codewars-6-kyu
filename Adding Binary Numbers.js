f8e984cf0c727cc552bee690c7297dc41a586668
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-10-16 23:55:48 -0300

    Update Adding Binary Numbers.js

--- Adding Binary Numbers.js
+++ Adding Binary Numbers.js
@@ -13,3 +13,15 @@ add('1101','101'); => '10010'
 add('1101','10111') => '100100'
 */
 
+function add(a,b){
+  let res = '', c = 0;
+  a = a.split('');
+  b = b.split('');
+  while (a.length || b.length || c) {
+    c += ~~a.pop() + ~~b.pop();
+    res = c % 2 + res;
+    c = c > 1;
+  }
+  res = res.replace(/^0+/, '');
+  return res || '0';
+};

f3670a38fbc91ae9c07a5dd10ce4afce45b9c7fe
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-10-17 22:59:12 -0300

    Update Adjacent pairs in a string.js

--- Adjacent pairs in a string.js
+++ Adjacent pairs in a string.js
@@ -18,3 +18,11 @@ Examples
 "cat cat dog dog cat cat"  -->  3
 */
 
+function countAdjacentPairs(searchString) {
+  let obj = {};
+  searchString
+    .toLowerCase()
+    .split(" ")
+    .map(v => (obj[v] = obj[v] ? obj[v] + 1 : 1));
+  return Object.values(obj).filter(v => v > 1).length;
+}

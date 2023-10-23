13831c3b355063fdc37e80968c03d875da771a32
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-10-22 22:18:42 -0300

    Update Alphabetized.js

--- Alphabetized.js
+++ Alphabetized.js
@@ -11,3 +11,10 @@ alphabetized("The Holy Bible") // "BbeehHilloTy"
 Inspired by Tauba Auerbach
 */
 
+function alphabetized(s) {
+  let ans = "";
+  for (let i = 97; i < 123; ++i)
+    for (let j = 0; j < s.length; j++)
+      if (s[j].toLowerCase().charCodeAt() == i) ans += s[j];
+  return ans;
+}

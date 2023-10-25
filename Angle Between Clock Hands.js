100bd5d2df19bdefd99bfd9b9a47f0d6fd76b425
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-10-24 23:09:49 -0300

    Update Angle Between Clock Hands.js

--- Angle Between Clock Hands.js
+++ Angle Between Clock Hands.js
@@ -14,3 +14,11 @@ at 6:00 the angle is: π (180 degrees)
 at 9:00 the angle is: π/2 (90 degrees)
 */
 
+function handAngle (date) {
+  let pi2 = Math.PI*2;
+  let m = date.getMinutes()/60, h = (date.getHours()+m)/12;
+  
+  let angle = Math.abs(h-m)*pi2;
+  return Math.min( angle, pi2-angle )
+
+}

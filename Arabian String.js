965d877f2e97b46c0ad8813d1d624a622ffec663
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-11-02 21:59:56 -0300

    Create Arabian String.js

---
+++ Arabian String.js
@@ -0,0 +1,11 @@
+/*
+DESCRIPTION:
+You must create a method that can convert a string from any format into CamelCase. This must support symbols too.
+
+Don't presume the separators too much or you could be surprised.
+Tests
+camelize("example name")   // => ExampleName
+camelize("your-NaMe-here") // => YourNameHere
+camelize("testing ABC")    // => TestingAbc
+*/
+

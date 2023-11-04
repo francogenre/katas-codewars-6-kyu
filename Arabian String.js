419beb86f1459eb125fe0f300979e818213e8998
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-11-03 22:18:52 -0300

    Update Arabian String.js

--- Arabian String.js
+++ Arabian String.js
@@ -9,3 +9,8 @@ camelize("your-NaMe-here") // => YourNameHere
 camelize("testing ABC")    // => TestingAbc
 */
 
+function camelize(str) {
+  return str.replace(/[^a-z0-9]/gi, " ").split` `.map(
+    v => v.slice(0, 1).toUpperCase() + v.slice(1).toLowerCase()
+  ).join``;
+}

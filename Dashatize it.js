835a9802525b6211a0045168ebfc9c017f25f90c
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-11-11 23:15:12 -0300

    Update Dashatize it.js

--- Dashatize it.js
+++ Dashatize it.js
@@ -7,3 +7,10 @@ dashatize(274) -> '2-7-4'
 dashatize(6815) -> '68-1-5'
 */
 
+
+function dashatize(num) {
+  return String(num)
+    .replace(/([13579])/g, "-$1-")
+    .replace(/--+/g, "-")
+    .replace(/(^-|-$)/g, "");
+}
